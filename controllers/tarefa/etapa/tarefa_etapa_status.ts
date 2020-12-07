2394558b3104064693c169016e0837751b823280
DanielAraujo-dac <danieldearaujocosta@gmail.com>
2020-12-07 11:46:45 -0400

    Adiciona CRUDs de Tarefas e Pastas

---
+++ controllers/tarefa/etapa/tarefa_etapa_status.ts
@@ -0,0 +1,104 @@
+import Controller, { Delete, Get, Post, Put, Route } from "@config/controller";
+import { Request, Response } from "express";
+import { QueryTypes } from "sequelize";
+
+@Route("/api/tarefa/etapa/status")
+export class ApiTarefaEtapaStatus extends Controller {
+
+  @Get("")
+  async listar (_req: Request, res: Response): Promise<Response> {
+    try {
+      const sql: any = await this.faina().query(`
+          SELECT *
+            FROM tarefa_etapa_status
+           WHERE tarefa_etapa_status.excluido_em IS NULL
+      `, {
+        type: QueryTypes.SELECT
+      });
+
+      return res.json({
+        sql: sql
+      });
+    } catch (e) {
+      return res.json({sucesso: false, mensagem: e.message, e: e});
+    }
+  }
+
+  @Post("")
+  async insert (req: Request, res: Response): Promise<Response> {
+    try {
+      const param = req.body;
+      const registro: any = await this.faina().query(`
+        INSERT
+          INTO tarefa_etapa_status
+             ( nome
+             , descricao
+             )
+        VALUES
+             ( :nome
+             , :descricao
+             )
+        `, {
+          replacements: {
+            nome: param.nome,
+            descricao: param.descricao
+          }
+        }
+      );
+      return res.json(registro);
+    } catch (e) {
+      return res.json({sucesso: false, mensagem: e.message, e: e});
+    }
+  }
+
+  @Put("")
+  async alterar (req: Request, res: Response): Promise<Response> {
+    try {
+      const param = req.body;
+      console.log(param);
+      const registro: any = await this.faina().query(`
+        UPDATE tarefa_etapa_status
+           SET nome        = :nome
+             , descricao   = :descricao
+             , alterado_id = :alteradoId
+             , alterado_em = :alteradoEm
+         WHERE id = :id
+        `, {
+          replacements: {
+            id: param.id,
+            nome: param.nome,
+            descricao: param.descricao,
+            alteradoId: param.alteradoId,
+            alteradoEm: param.alteradoEm
+          }
+        }
+      );
+      return res.json(registro);
+    } catch (e) {
+      return res.json({sucesso: false, mensagem: e.message, e: e});
+    }
+  }
+
+  @Delete("/:id")
+  async excluir (req: Request, res: Response): Promise<Response> {
+    try {
+      const registro: any = await this.faina().query(`
+        UPDATE tarefa_etapa_status
+           SET excluido_em  = CURRENT_TIMESTAMP()
+             , excluido_id  = :userId
+         WHERE id           = ${req.query.id}
+           AND excluido_em IS NULL
+        `, {
+          replacements: {
+            userId: 0
+          }
+        }
+      );
+      return res.json(registro);
+    } catch (e) {
+      return res.json({sucesso: false, mensagem: e.message, e: e});
+    }
+  }
+}
+
+export default ApiTarefaEtapaStatus;
---
+++ controllers/tarefa/tarefa.ts
@@ -0,0 +1,170 @@
+import Controller, { Delete, Get, Post, Put, Route } from "@config/controller";
+import { Request, Response } from "express";
+import { QueryTypes } from "sequelize";
+
+@Route("/api/pasta")
+export class ApiPasta extends Controller {
+
+  @Get("/qualquercoisa")
+  async qq (_req: Request, res: Response): Promise<Response> {
+    return res.json({
+      qualquercoisa: "Ok"
+    });
+  }
+
+  @Get("")
+  async listar (_req: Request, res: Response): Promise<Response> {
+    console.log(_req.body);
+    const sql: any = await this.faina().query(`
+        SELECT *
+          FROM pasta
+         WHERE pasta.excluido_em IS NULL
+    `, {
+      type: QueryTypes.SELECT
+    });
+
+    return res.json({
+      sql: sql
+    });
+  }
+
+  @Post("")
+  async insert (req: Request, res: Response): Promise<Response> {
+    try {
+      const param = req.body;
+      let pasta, pastaPasta;
+      await this.faina().transaction(async (t) => {
+        pasta = await this.faina().query(`
+              /* Inserir a Pasta */
+          INSERT
+            INTO pasta
+               ( tipo
+               , projeto_id
+               , nome
+               , descricao
+               )
+          VALUES
+               ( :tipo
+               , :projetoId
+               , :nome
+               , :descricao
+               );`, {
+          type: QueryTypes.INSERT,
+          replacements: {
+            tipo: param.tipo,
+            projetoId: param.projetoId,
+            nome: param.nome,
+            descricao: param.descricao
+          },
+          transaction: t
+        });
+
+        if (param.pastaId) {
+          pastaPasta = await this.faina().query(`
+                /* Inserir o Relacionamento da Pasta com a Pasta Mãe */
+            INSERT
+              INTO pasta_pasta
+                 ( mae_id
+                 , filha_id
+                 , incluido_id
+                 )
+            SELECT pasta.id
+                 , LAST_INSERT_ID()
+                 , ${0}
+              FROM pasta
+             WHERE pasta.id = ${param.pastaId};`, {
+            type: QueryTypes.INSERT,
+            transaction: t
+          });
+        }
+      });
+
+      const registro = {
+        pasta: pasta,
+        pastaPasta: pastaPasta
+      };
+
+      return res.json(registro);
+    } catch (e) {
+      return res.json({sucesso: false, mensagem: e.message, e: e});
+    }
+  }
+
+  @Put("")
+  async alterar (req: Request, res: Response): Promise<Response> {
+    try {
+      const param = req.body;
+      let pasta, pastaPasta;
+
+      await this.faina().transaction(async (t) => {
+        pasta = await this.faina().query(`
+              /* Altera informações sobre a pasta */
+          UPDATE pasta
+             SET tipo        = :tipo
+               , projeto_id  = :projetoId
+               , nome        = :nome
+               , descricao   = :descricao
+               , alterado_id = :alteradoId
+               , alterado_em = :alteradoEm
+           WHERE excluido_em IS NULL
+             AND id = :id`, {
+          type: QueryTypes.UPDATE,
+          replacements: {
+            id: param.id,
+            tipo: param.tipo,
+            projetoId: param.projetoId,
+            nome: param.nome,
+            descricao: param.descricao,
+            alteradoId: param.alteradoId,
+            alteradoEm: param.alteradoEm
+          },
+          transaction: t
+        });
+
+        if (param.pastaId && param.pastaIdNovo) {
+          pastaPasta = await this.faina().query(`
+                /* Altera o Relacionamento da Pasta com a Pasta Mãe */
+            UPDATE pasta_pasta
+               SET mae_id  = ${param.pastaIdNovo}
+             WHERE fila_id = ${param.Id}
+               AND mae_id  = ${param.pastaId};`, {
+            type: QueryTypes.UPDATE,
+            transaction: t
+          });
+        }
+      });
+
+      const registro = {
+        pasta: pasta,
+        pastaPasta: pastaPasta
+      };
+
+      return res.json(registro);
+    } catch (e) {
+      return res.json({sucesso: false, mensagem: e.message, e: e});
+    }
+  }
+
+  @Delete("/:id")
+  async excluir (req: Request, res: Response): Promise<Response> {
+    try {
+      const registro: any = await this.faina().query(`
+        UPDATE pasta
+           SET excluido_em  = CURRENT_TIMESTAMP()
+             , excluido_id  = :userId
+         WHERE id           = ${req.query.id}
+           AND excluido_em IS NULL
+        `, {
+          replacements: {
+            userId: 0
+          }
+        }
+      );
+      return res.json(registro);
+    } catch (e) {
+      return res.json({sucesso: false, mensagem: e.message, e: e});
+    }
+  }
+}
+
+export default ApiPasta;
